389a29dc2db300b999cdebaf741159e8f82f8fb5
juannn91 <juannn91@gmail.com>
2012-10-26 10:31:13 -0500

    otro intento

---
+++ personeria/docs 2/globals.py
@@ -0,0 +1,42 @@
+ASUNTOS = (
+ ('DC','declaracion'),
+ ('RN','remision de novedades'),
+ ('DP','envio de derecho de peticion'),
+ ('RR','recibo de respuesta'),
+)
+
+ESTADO = (
+ ('PRO','proceso'),
+ ('APR','aprobado'),
+ ('FAL','no aprobado'),
+)
+
+INVESTIGACIONES = (
+ ('IP','investigacion preliminar'),
+ ('ID','investigacion disciplinaria'),
+ ('IP','investigacion de pruebas'),
+ ('TC','translado de conclusion'),
+)
+
+NOTIFICACIONES = (
+ ('PN','personalmente'),
+ ('AD','audiencia'),
+ ('PE','por estados'),
+ ('ED','edito'),
+)
+
+TERMINO_DE_COTESTACION = (
+ ('TUT','Tutela'),
+ ('DES','Desacatos'),
+ ('PET','Peticiones'),
+ ('PRO','Proceso diciplinario'),
+ ('OFI','Oficios'),
+)
+
+
+
+
+
+
+
+
